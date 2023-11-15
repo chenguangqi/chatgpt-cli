391de3209979fb071bfe5970cce62c25aa0a4bc2
chen <348249063@qq.com>
2023-11-15 14:35:06 +0800

    修改配置文件路径

--- chatgpt_cli/settings.py
+++ chatgpt_cli/settings.py
@@ -4,6 +4,6 @@ import configparser
 settings = configparser.ConfigParser()
 configs = [
     os.path.join(os.getcwd(), 'config.ini'),
-    os.path.join(os.getenv('USERPROFILE') or os.getenv('HOME'), 'chatgpt_cli', 'config.ini')
+    os.path.join(os.getenv('USERPROFILE') or os.getenv('HOME'), 'chatgpt', 'config.ini')
 ]
 filenames = settings.read(configs, encoding='UTF-8')
